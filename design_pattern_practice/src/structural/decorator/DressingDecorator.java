263457c5c4495ad9aef019e848395678e8e1d24a
Ahmed Gharib <ahmedgharib0014@gmail.com>
2022-11-21 19:33:28 +0200

    feat:decortor pattern example

---
+++ design_pattern_practice/src/structural/decorator/DressingDecorator.java
@@ -0,0 +1,16 @@
+package structural.decorator;
+
+public class DressingDecorator extends SandwichDecorator{
+    public DressingDecorator(Sandwich customSandwich) {
+        super(customSandwich);
+    }
+
+    @Override
+    public String make() {
+        return super.make() + addDressing();
+    }
+
+    private String addDressing() {
+        return " + dressing";
+    }
+}
---
+++ design_pattern_practice/src/structural/decorator/MeatDecorator.java
@@ -0,0 +1,16 @@
+package structural.decorator;
+
+public class MeatDecorator extends SandwichDecorator{
+    public MeatDecorator(Sandwich customSandwich) {
+        super(customSandwich);
+    }
+
+    @Override
+    public String make() {
+        return super.make() + addMeat();
+    }
+
+    private String addMeat() {
+        return " + meat";
+    }
+}
