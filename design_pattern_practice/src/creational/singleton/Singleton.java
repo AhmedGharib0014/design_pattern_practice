65cd77b6b4a6254577336b5370e04de3cc86f149
Ahmed Gharib <ahmedgharib0014@gmail.com>
2023-02-25 00:57:24 +0200

    feat: make it thread save

--- design_pattern_practice/src/creational/singleton/Singleton.java
+++ design_pattern_practice/src/creational/singleton/Singleton.java
@@ -2,14 +2,23 @@ package creational.singleton;
 
 public class Singleton {
 
-    private static Singleton instance;
+    private static volatile Singleton instance;
 
-    private Singleton(){}
+    private Singleton(){
+        if(instance != null) {
+            throw new RuntimeException("use getInstance method");
+        }
+    }
 
 
     public static Singleton getInstance(){
-        if(instance == null){
-            instance = new Singleton();
+        if(instance == null) {
+            synchronized (Singleton.class){
+                if(instance == null){
+                    instance = new Singleton();
+
+                }
+            }
         }
 
         return instance;
